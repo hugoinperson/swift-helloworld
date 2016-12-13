ba55ef0c4479882b0e13ee8c8bedf2fe554003f8
hugoinperson <hsuyachieh@gmail.com>
2016-12-13 15:43:46 -0600

    finish Generics

---
+++ hellowrold.playground/Pages/generics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+var str = "Hello, playground"
+
+// write a name inside angle brackets to make a generic function or type.
+func makeArray<Item>(repeating item: Item, numberOfTimes: Int) -> [Item] {
+    var result = [Item]()
+    for _ in 0..<numberOfTimes {
+        result.append(item)
+    }
+    return result
+}
+makeArray(repeating: "knock", numberOfTimes:4);
+makeArray(repeating: 56, numberOfTimes:4);
+
+
+// Reimplement the Swift standard library's optional type
+enum OptionalValue<Wrapped> {
+    case none
+    case some(Wrapped)
+}
+var possibleInteger: OptionalValue<Int> = .none;
+possibleInteger = .some(100);
+
+
+// use where right before the body to specify a list of requirements
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U) -> Bool
+    where T.Iterator.Element: Equatable, T.Iterator.Element == U.Iterator.Element {
+        for lhsItem in lhs {
+            for rhsItem in rhs {
+                if lhsItem == rhsItem {
+                    return true
+                }
+            }
+        }
+        return false
+}
+anyCommonElements([1, 2, 3], [3])
